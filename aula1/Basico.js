2415158f7e7962a37a433d4cd343d2ba664a789e
isavn <isabellavieiraneves@gmail.com>
2026-03-19 22:22:44 -0300

    aula sobre variáveis no js (aula 19/03)

---
+++ aula1/Basico.js
@@ -0,0 +1,15 @@
+// javascript é uma linguagem fracamente tipada
+
+var variavelVar = "isabella"
+let variavellet = "Viera"
+const variavelconst = 2009 
+
+console.log ("Meu nome é");
+console.log (variavelVar);
+console.log (variavelVar,variavellet);
+console.log (variavelVar+variavellet);
+
+
+
+variavellet = 6
+console.log (variavellet);
