4f5abb314e859ea26ec78f3f1a774a9045707840
isavn <isabellavieiraneves@gmail.com>
2026-04-09 21:39:21 -0300

    exercíciocorrigido

--- exerciciosts/exercicio1.ts
+++ exerciciosts/exercicio1.ts
@@ -2,9 +2,7 @@
 const cidades: string[] = ['São Paulo', 'Rio de Janeiro', 'Curitiba', 'Ouro Preto', 'Salvador'];
 
 function listarCidades(listaDeCidades: string[]): void {
-    for (let i: number = 0; i < listaDeCidades.length; i++) {
-        console.log("Cidade " + (i + 1) + ": " + listaDeCidades[i]);
-    }
+  cidades.forEach(cidades=> console.log(cidades));
 }
 
 listarCidades(cidades);
